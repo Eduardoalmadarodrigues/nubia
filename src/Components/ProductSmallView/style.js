08c82a105dd21b1fa5453713e4911e44cf48d6f1
Wilson Oliveira <94942615+noziXsHooter@users.noreply.github.com>
2022-11-30 14:36:21 -0300

    fix

--- src/Components/ProductSmallView/style.js
+++ src/Components/ProductSmallView/style.js
@@ -46,23 +46,3 @@ export const PaymentWays = styled.span`
     padding-left: 10px;
     font-size: small;
 `;
-
-/*
-## # THE LAST STYLES
-export const ProductWrapper = styled.div`
-    height: 700px;
-    max-width: 500px;
-    min-width: 500px;
-    border: 1px solid black ;
-    margin: 50px ;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-`;
-
-export const ProductImage = styled.img`
-    width: 400px ;
-    height: 400px ;`
-
-export const ProductName = styled.h1``;
