88637b6831dc9cc618081dd09445f1ce42a187b6
Wilson Oliveira <94942615+noziXsHooter@users.noreply.github.com>
2022-11-30 14:29:30 -0300

    Style changes

--- src/Components/AddToCartButton/style.js
+++ src/Components/AddToCartButton/style.js
@@ -1,6 +1,22 @@
 import styled from 'styled-components'
 
 export const AddButton = styled.button`
+    margin: 10px 5px 10px 2px;
     width: 100px;
-    height: 100px;
+    height: 50px;
+    border-radius: 5px;
+    border: none;
+    color: white;
+    background-color: #f57a27;
+    font-size: medium;
+    padding: 3px;
+    text-align: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    flex: 0 0 0px;
+    margin-left: auto;
+    margin-right: auto;
+    align-self: flex-start;
 `;
