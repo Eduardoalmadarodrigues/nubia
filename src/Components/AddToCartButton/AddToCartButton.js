f74e759fd1452852da1a2937a5cea60e46273a11
Wilson Oliveira <94942615+noziXsHooter@users.noreply.github.com>
2022-11-30 14:38:27 -0300

    Change buttom name

--- src/Components/AddToCartButton/AddToCartButton.js
+++ src/Components/AddToCartButton/AddToCartButton.js
@@ -5,6 +5,6 @@ import { AddButton } from './style'
 export function AddToCartButton(props){
     const {userDispatch} = useContext(UserContext);
     return(
-        <AddButton onClick={()=>userDispatch({type : 'addToCart' , toCart: props.product.name})}>ADDtoCart</AddButton>
+        <AddButton onClick={()=>userDispatch({type : 'addToCart' , toCart: props.product.name})}>Add to Cart</AddButton>
     );
 };
