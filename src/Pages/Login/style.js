4e837b78580f82a34d7ff73ae3982e57c3219d21
Wilson Oliveira <94942615+noziXsHooter@users.noreply.github.com>
2022-12-01 01:43:48 -0300

    Login styles changed

--- src/Pages/Login/style.js
+++ src/Pages/Login/style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { ReactComponent as Icon } from "../../Assets/logo.svg";
 
 export const Wrapper = styled.div`
     width: 100vw;
@@ -6,7 +7,7 @@ export const Wrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: antiquewhite;
+    background-color: #0d1117;
 `
 
 export const FormWrapper = styled.div`
@@ -14,22 +15,54 @@ export const FormWrapper = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    width: 200px;
-    height: 300px;
+    width: 300px;
+    height: 400px;
+    border-radius: 5px;
     border: 1px solid black;
+    background-color: #161b22;
+`;
+
+export const Logo = styled(Icon)`
+
+    margin: 10px;
+    width: 100px;
+    height: 100px ;
+
 `;
 
 export const EmailInput = styled.input`
-    width: 150px;
-    height: 20px;
+    width: 200px;
+    height: 40px;
+    margin: 10px;
+    color: gray;
+    font-size: large;
+    background-color: #0d1117;
+    border: 2px solid #f57a27;
+    border-radius: 6px;
 `;
 
 export const PasswordInput = styled.input`
-    width: 150px;
-    height: 20px;
+    width: 200px;
+    height: 40px;
+    margin: 10px;
+    color: gray;
+    font-size: large;
+    background-color: #0d1117;
+    border: 2px solid #f57a27;
+    border-radius: 6px;
 `
 
 export const SubmitButton = styled.button`
     width: 150px;
     height: 40px;
+    width: 100px;
+    height: 50px;
+    margin-top: 40px;
+    border-radius: 5px;
+    border: none;
+    color: white;
+    background-color: #f57a27;
+    font-size: medium;
+    padding: 3px;
+
 `;
