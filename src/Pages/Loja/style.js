a0ad27d6dfb5fb2ae1e60328e079a2463673cad7
Wilson Oliveira <94942615+noziXsHooter@users.noreply.github.com>
2022-11-30 23:55:19 -0300

    Import cart.svg and Search Input

--- src/Pages/Loja/style.js
+++ src/Pages/Loja/style.js
@@ -3,29 +3,58 @@ import { ReactComponent as Icon } from "../../Assets/logo.svg";
 import { ReactComponent as Menu } from "../../Assets/menu_buttom.svg";
 import { ReactComponent as Home } from "../../Assets/home.svg";
 import { ReactComponent as User } from "../../Assets/user.svg";
+import { ReactComponent as Cart } from "../../Assets/cart.svg";
 
 export const Wrapper = styled.div`
     width: 100%;
     background-color: #161b22; 
 `
 
-export const Header = styled.div`
+export const Header = styled.div` //COMMIT
     display: flex ;
     align-items: center ;
+    justify-content: center;
     width: 100% ;
     height: 120px ;
     background-color: #0d1117;
 `;
 
 export const Logo = styled(Icon)`
-width: 100px;
-height: 100px ;
-`
 
-export const NavLink = styled.div`
-    font-size: 25px;
-    padding: 15px;
+    margin: 10px;
+    width: 40px;
+    height: 40px ;
+
 `;
+
+
+export const MenuButton = styled(Menu)`
+
+    padding: 30px;
+    width: 40px;
+    height: 40px ;
+    justify-content: start;
+    align-items: start;
+`;
+
+export const NavLink = styled.div`  //COMMIT
+    width: 60px;
+    height: 20px;
+    border-radius: 5px;
+    border: none;
+    color: white;
+    background-color: #f57a27;
+    font-size: medium;
+    padding: 3px;
+    text-align: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    flex: 0 0 0px;
+    margin: 20px;
+`;
+
 
 export const ProductsWrapper = styled.div`
     max-width: 100%;
@@ -35,11 +64,23 @@ export const ProductsWrapper = styled.div`
     justify-content: center ;
 `;
 
-export const MenuButton = styled(Menu)`
-    padding: 30px;
-    width: 40px;
-    height: 40px ;
-    `;
+export const SearchInput = styled.input`
+    width: 82%;
+    height: 40px;
+    margin: auto;
+    margin-top: 10px;
+    color: gray;
+    font-size: large;
+    background-color: #0d1117;
+    border: 2px solid gray;
+    border-radius: 6px;
+    display: flex;
+    overflow: auto;
+    flex-wrap: column;
+    align-items: center;
+    justify-content: center ;
+`;
+
 export const HomeButtom = styled(Home)`
     width: 30px;
     height: 25px;
@@ -72,4 +113,21 @@ export const UserButtom = styled(User)`
     align-items: center;
     flex: 0 0 0px;
     align-self: flex-start;
+`;
+
+export const CartButtom = styled(Cart)` //COMMIT
+    width: 30px;
+    height: 25px;
+    border-radius: 5px;
+    border: none;
+    color: white;
+    font-size: medium;
+    padding: 3px;
+    text-align: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    flex: 0 0 0px;
+    align-self: center;
 `;
