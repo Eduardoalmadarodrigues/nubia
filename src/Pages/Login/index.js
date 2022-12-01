0d8aad5c5d0c44041f9c3e0364f7002b8ecaf6ce
Wilson Oliveira <94942615+noziXsHooter@users.noreply.github.com>
2022-12-01 01:44:56 -0300

    Import Logo

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,5 +1,5 @@
 import React , {useContext, useState} from 'react';
-import { EmailInput, FormWrapper, PasswordInput, SubmitButton, Wrapper } from './style';
+import { EmailInput, FormWrapper, PasswordInput, SubmitButton, Wrapper, Logo } from './style';
 import { UserContext } from '../../Contexts/UserContext';
 import {Link} from 'react-router-dom'
 
@@ -10,6 +10,7 @@ export default function Login(){
     return (
     <Wrapper>
         <FormWrapper>
+            <Logo></Logo>
             <EmailInput value={emailInput} onChange={(e)=>{
                 changeEmailInput(e.target.value);
             }} placeholder="Email"/>
